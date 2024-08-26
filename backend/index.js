25b0eab56c0160ceefc54c0c49a99393ea0ab90b
Maneesh Shukla <shuklamaneesh24@gmail.com>
2024-08-26 17:25:04 +0530

    Deployment fixes

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import connectDB from "./src/db/index.js";
+import { app } from "./src/app.js";
+
+dotenv.config({
+    path:'/.env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Post is lsitening at ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed!!!",err);
+})
+
+
