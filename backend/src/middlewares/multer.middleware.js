c23da3cd39aac2f4266cb90e2eedb720802e4800
Maneesh Shukla <shuklamaneesh24@gmail.com>
2024-08-27 13:57:35 +0530

    Deployment fixes

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,4 +1,10 @@
 import multer from "multer";
+import fs from 'fs';
+
+const tempDir = './public/temp';
+if (!fs.existsSync(tempDir)){
+    fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
